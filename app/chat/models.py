e787bf7a159e45ba32d0e38b492bc187ce1d268e
Abdelwahab Hamada <abdelwahab.hamada@gmail.com>
2024-07-07 15:19:50 +0300

    feat: chat, message, attachments store to db

--- app/chat/models.py
+++ app/chat/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel, SoftDeletableModel
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth import get_user_model
+from typing import Optional, Any
+from django.db.models import Q
+import uuid
+import os
 
-# Create your models here.
 
-class ChatRoom(TimeStampedModel):
+UserModel: AbstractBaseUser = get_user_model()
+
+def user_directory_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    return settings.DEFAULT_FILES_PATH.format(uuid=instance.id, extension=extension)
+
+class Chat(TimeStampedModel):
     id = models.BigAutoField(primary_key=True, verbose_name=_("Id"))
     user1 = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("User1"),
                               related_name="+", db_index=True)
@@ -11,59 +25,72 @@ class ChatRoom(TimeStampedModel):
 
     class Meta:
         unique_together = (('user1', 'user2'), ('user2', 'user1'))
-        verbose_name = _("Dialog")
-        verbose_name_plural = _("Dialogs")
+        verbose_name = _("Chat")
+        verbose_name_plural = _("Chats")
 
     def __str__(self):
-        return _("Dialog between ") + f"{self.user1_id}, {self.user2_id}"
+        return _("Chat between ") + f"{self.user1_id}, {self.user2_id}"
 
     @staticmethod
-    def dialog_exists(u1: AbstractBaseUser, u2: AbstractBaseUser) -> Optional[Any]:
-        return ChatRoom.objects.filter(Q(user1=u1, user2=u2) | Q(user1=u2, user2=u1)).first()
+    def _exists(u1: AbstractBaseUser, u2: AbstractBaseUser) -> Optional[Any]:
+        return Chat.objects.filter(Q(user1=u1, user2=u2) | Q(user1=u2, user2=u1)).first()
 
     @staticmethod
     def create_if_not_exists(u1: AbstractBaseUser, u2: AbstractBaseUser):
-        res = ChatRoom.dialog_exists(u1, u2)
-        if not res:
-            ChatRoom.objects.create(user1=u1, user2=u2)
+        chat = Chat._exists(u1, u2)
+        if not chat:
+            chat = Chat.objects.create(user1=u1, user2=u2)
+
+        return chat
 
     @staticmethod
-    def get_dialogs_for_user(user: AbstractBaseUser):
-        return ChatRoom.objects.filter(Q(user1=user) | Q(user2=user)).values_list('user1__pk', 'user2__pk')
+    def get_chats_for_user(user: AbstractBaseUser):
+        return Chat.objects.filter(Q(user1=user) | Q(user2=user)).values_list('user1__pk', 'user2__pk')
 
 
 class ChatMessage(TimeStampedModel, SoftDeletableModel):
     id = models.BigAutoField(primary_key=True, verbose_name=_("Id"))
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("Author"),
                                related_name='from_user', db_index=True)
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_("Recipient"),
-                                  related_name='to_user', db_index=True)
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
     text = models.TextField(verbose_name=_("Text"), blank=True)
-    file = models.ForeignKey(UploadedFile, related_name='message', on_delete=models.DO_NOTHING,
-                             verbose_name=_("File"), blank=True, null=True)
-
     read = models.BooleanField(verbose_name=_("Read"), default=False)
     all_objects = models.Manager()
 
     @staticmethod
-    def get_unread_count_for_dialog_with_user(sender, recipient):
-        return ChatMessage.objects.filter(sender_id=sender, recipient_id=recipient, read=False).count()
+    def get_unread_count_for_chat_with_user(sender, chat):
+        return ChatMessage.objects.filter(sender_id=sender, chat_id=chat, read=False).count()
 
     @staticmethod
-    def get_last_message_for_dialog(sender, recipient):
-        return ChatMessage.objects.filter(
-            Q(sender_id=sender, recipient_id=recipient) | Q(sender_id=recipient, recipient_id=sender)) \
-            .select_related('sender', 'recipient').first()
+    def get_last_message_for_chat(chat):
+        return ChatMessage.objects.filter(chat_id=chat).select_related('sender', 'chat').first()
+
+    @staticmethod
+    def create_first_message(sender: AbstractBaseUser, recipient: AbstractBaseUser, text: str=None):
+        chat = Chat.create_if_not_exists(self.sender, self.recipient)
+        message = ChatMessage.objects.create(sender=sender, chat=chat, text=text)
+
+        return message
 
     def __str__(self):
         return str(self.pk)
-
-    def save(self, *args, **kwargs):
-        super(ChatMessage, self).save(*args, **kwargs)
-        ChatRoom.create_if_not_exists(self.sender, self.recipient)
 
     class Meta:
         ordering = ('-created',)
         verbose_name = _("Message")
         verbose_name_plural = _("Messages")
+
  
+class MessageAttachment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
+    file = models.FileField(verbose_name=_("File"), blank=False, null=False, upload_to=user_directory_path)
+    message = models.ForeignKey(ChatMessage, related_name='file', on_delete=models.CASCADE,
+                            blank=True, null=True)
+
+    @staticmethod
+    def create_first_message_attachment(sender: AbstractBaseUser, recipient: AbstractBaseUser, file, text: str=None):
+        message = ChatMessage.create_first_message(sender=sebder, recipient=recipient, text=text)
+        MessageAttachment.objects.create(file=file, message=message)
+
+    def __str__(self):
+        return str(self.file.name)
