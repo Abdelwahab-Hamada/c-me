a0f40cc59c48a17d5b8414ac919d085245a11a08
merrsamira <125443409+merrsamira@users.noreply.github.com>
2024-07-10 23:56:46 +0100

    Generic display views

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -2,8 +2,20 @@ from django.urls import path
 
 from . import views
 
+from chat.views import ChatDetailView
+
+from chat.views import ChatListView
+
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("<str:room_name>/", views.room, name="room"),
+]
+
+urlpatterns = [
+    path("<slug:slug>/", ChatDetailView.as_view(), name="chat-detail"),
+]
+
+urlpatterns = [
+    path("", ChatListView.as_view(), name="chat-list"),
 ]
--- app/chat/views.py
+++ app/chat/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 
+from django.utils import timezone
+from django.views.generic.detail import DetailView
+
+from chat.models import ChatMessage
+
+from django.views.generic.list import ListView
+
 
 def index(request):
     return render(request, "chat/index.html")
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+
+
+class ChatDetailView(DetailView):
+    model = ChatMessage
+    template_name = 'chat/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+
+class ChatListView(ListView):
+    model = ChatMessage
+    paginate_by = 10  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
