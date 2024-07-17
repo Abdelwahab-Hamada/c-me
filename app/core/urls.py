3edc8c904977de87568c9051df5522f38b44b182
merrsamira <125443409+merrsamira@users.noreply.github.com>
2024-07-17 23:18:38 +0100

    Refactored chat list with Tailwind CSS

--- app/core/urls.py
+++ app/core/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def redirect_to_chat(request):
+    return redirect('/chat/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("chat/", include("chat.urls")),
+    path('chat/', include('chat.urls')),
+    path('', redirect_to_chat),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
